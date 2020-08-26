caf800cd2469bed32b9c7e9a279706deca0c9c40
Ousmane <nouroudrame96@gmail.com>
2020-08-26 23:55:33 +0200

    Test

--- fightSimulator.js
+++ fightSimulator.js
@@ -70,5 +70,13 @@ class Guerrier extends Personnage{
 }
 
 
-
+var gandalf = new Magicien('Gandalf');
+var thor    = new Guerrier('Thor');
+console.log(thor.informations);
+console.log(gandalf.informations);
+gandalf.attaquer(thor);
+console.log(thor.informations);
+thor.attaquer(gandalf);
+console.log(gandalf.informations);
+gandalf.coupSpecial(thor);
 
